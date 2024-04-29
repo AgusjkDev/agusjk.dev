7e007bb19eefca489a10c530a77c7cf2b3d63ef8
AgusjkDev <agusjkdev7@gmail.com>
2024-04-29 00:56:29 -0300

    add call to action component

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -17,6 +17,19 @@ export default {
                 },
                 border: "var(--border)",
             },
+            animation: {
+                "background-shine": "background-shine 2s linear infinite",
+            },
+            keyframes: {
+                "background-shine": {
+                    from: {
+                        backgroundPosition: "0 0",
+                    },
+                    to: {
+                        backgroundPosition: "-200% 0",
+                    },
+                },
+            },
         },
     },
     plugins: [],
