a44116eea6cce9a4035b0471d294325dd2876d03
AgusjkDev <agusjkdev7@gmail.com>
2024-04-30 20:30:07 -0300

    remove astro-icons

--- src/config.ts
+++ src/config.ts
@@ -3,19 +3,19 @@ export const siteConfig = {
     email: "agusjkdev7@gmail.com",
     socials: [
         {
+            key: "twitter",
             label: "Twitter",
             href: "https://twitter.com/agusjkdev",
-            icon: "twitter",
-        },
+        } as const,
         {
+            key: "github",
             label: "GitHub",
             href: "https://github.com/agusjkdev",
-            icon: "github",
-        },
+        } as const,
         {
+            key: "linkedin",
             label: "LinkedIn",
             href: "https://www.linkedin.com/in/agusjkdev",
-            icon: "linkedin",
-        },
+        } as const,
     ],
 };
