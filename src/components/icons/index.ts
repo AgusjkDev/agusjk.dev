d6105d9a45421a70ce387b82943287f9ad1bb3ee
AgusjkDev <agusjkdev7@gmail.com>
2024-04-30 21:14:41 -0300

    add icon type

--- src/components/icons/index.ts
+++ src/components/icons/index.ts
@@ -1,4 +1,8 @@
+import type { HTMLAttributes } from "astro/types";
+
+export interface Props extends HTMLAttributes<"svg"> {}
+export type IconType = (_props: Props) => any;
+
 export { default as Github } from "./github.astro";
 export { default as Linkedin } from "./linkedin.astro";
 export { default as Twitter } from "./twitter.astro";
-
