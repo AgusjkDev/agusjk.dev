0a2f5adff291a3e81e8d39cd05f58cb2f5d9ff9d
AgusjkDev <agusjkdev7@gmail.com>
2024-05-02 15:23:12 -0300

    add navigation to site config

--- src/config.ts
+++ src/config.ts
@@ -3,6 +3,20 @@ import * as SocialIcons from "@/components/icons/socials";
 export const siteConfig = {
     title: "Personal Website",
     email: "agusjkdev7@gmail.com",
+    navigation: [
+        {
+            label: "Home",
+            href: "/",
+        },
+        {
+            label: "Blog",
+            href: "/blog",
+        },
+        {
+            label: "About",
+            href: "/about",
+        },
+    ],
     socials: [
         {
             icon: SocialIcons.Twitter,
