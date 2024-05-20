2d754d1260f511a2547e44b365c1df398233ca11
AgusjkDev <agusjkdev7@gmail.com>
2024-05-20 19:33:46 -0300

    add this website as a project

--- src/components/icons/projects/index.ts
+++ src/components/icons/projects/index.ts
@@ -1,3 +1,4 @@
+export { default as AgusjkDev } from "./agusjk-dev.astro";
 export { default as DreamDuo } from "./dream-duo.astro";
 export { default as FavgenCli } from "./favgen-cli.astro";
 export { default as NextjsAppdirSnippets } from "./nextjs-appdir-snippets.astro";
